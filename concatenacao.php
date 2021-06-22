dced2c146851de8fc2c7feb5a66c13447b482f97
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-22 13:11:45 -0300

    Concatencao

---
+++ concatenacao.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Curso Php</title>
+    </head>
+
+    <body>
+        <?php
+            $nome = "Larissa";
+            
+
+            echo 'Ola Larissa emanola , tudo certo com vc , seu encosto??????';
+        ?>
+    </body>
+</html>
