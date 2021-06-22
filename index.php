174336e18e6bc3407f6998669237707ac50da3e1
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-22 11:04:43 -0300

    Primeira aula do curso de php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Curso Php</title>
+    </head>
+
+    <body>
+        <?php
+            echo 'Utilizando a tag padrão';
+        ?>
+        <br/>
+        <?= 'Utilizando a tag Impressão' ?>
+    </body>
+</html>
