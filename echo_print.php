e19282d0b7e0d5648b19f6ec8daaf79570ebe0d9
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-22 11:34:27 -0300

    Saída de dados com echo e print

---
+++ echo_print.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Curso Php</title>
+    </head>
+
+    <body>
+        <?php
+           echo 'Comando Echo<br/>';
+           print 'Comando Print';
+        ?>
+    </body>
+</html>
